996957146309081192d00a7668a38c14b50b4813
ElwynCruz <ElwynCruz@ucla.edu>
2021-02-05 16:53:45 -0800

    profile page, navbar and router

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,14 +4,14 @@ import 'firebaseui/dist/firebaseui.css'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDT2az1B1ZVbAGzxokouCmSXfU6cEPCxmk",
+    authDomain: "capitalist-hinge.firebaseapp.com",
+    databaseURL: "https://capitalist-hinge-default-rtdb.firebaseio.com",
+    projectId: "capitalist-hinge",
+    storageBucket: "capitalist-hinge.appspot.com",
+    messagingSenderId: "1024972331185",
+    appId: "1:1024972331185:web:f43e8a1356ca2fe4deea90",
+    measurementId: "G-0M52DMCW52"
 };
 firebase.initializeApp(firebaseConfig);
 
