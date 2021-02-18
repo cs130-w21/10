1813f5b92691e51ddb38d214d27b0a6a1299a6ec
Jordan Soon <jordansoon@ucla.edu>
2021-02-17 19:48:21 -0800

    Autocomplete with list of interests from DB working now

--- src/components/Profile/EditProfilePopup/EditProfilePopup.js
+++ src/components/Profile/EditProfilePopup/EditProfilePopup.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -8,21 +10,38 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 
 import { useAuth } from '../../../services/AuthContext';
+import { db } from '../../../services/firebase';
 
 const EditProfilePopup = ({ isOpen, onDismiss }) => {
   const { userData, updateUserData } = useAuth();
 
+  // State for list of interests
+  const [interestsOptions, setInterestsOptions] = useState([]);
+
+  // State related to form values
   const [personalInfo, setPersonalInfo] = useState({});
   const [contactInfo, setContactInfo] = useState({});
+  const [userInterests, setUserInterests] = useState([]);
 
   const resetFormValues = useCallback(() => {
     setPersonalInfo(userData.personalInfo);
     setContactInfo(userData.contactInfo);
+    setUserInterests(userData.interests ? userData.interests : []);
   }, [userData]);
 
+  // Every time modal dialog opens/closes, reset the form value state.
   useEffect(() => {
     resetFormValues();
   }, [resetFormValues, isOpen]);
+
+  // Get list of interests from db
+  useEffect(() => {
+    db.ref('interests/').on('value', (snapshot) => {
+      if (snapshot !== null) {
+        setInterestsOptions(snapshot.val());
+      }
+    });
+  }, []);
   
   const handlePersonalInfoChange = (field) => (e) => {
     setPersonalInfo({ ...personalInfo, [field]: e.target.value });
@@ -38,6 +57,7 @@ const EditProfilePopup = ({ isOpen, onDismiss }) => {
       ...userData,
       personalInfo,
       contactInfo,
+      interests: userInterests,
     });
   };
 
@@ -68,6 +88,24 @@ const EditProfilePopup = ({ isOpen, onDismiss }) => {
             label="LinkedIn"
             value={contactInfo.linkedin}
             onChange={handleContactInfoChange('linkedin')}
+          />
+          <Autocomplete
+            multiple
+            options={interestsOptions}
+            freeSolo
+            value={userInterests}
+            onChange={(e, newValue) => setUserInterests(newValue)}
+            renderTags={(interests, getTagProps) =>
+              interests.map((interest, index) => (
+                <Chip variant="outlined" label={interest} {...getTagProps({ index })} />
+              ))
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params} variant="outlined"
+                label="Interests" placeholder="Interests"
+              />
+            )}
           />
         </form>
       </DialogContent>
