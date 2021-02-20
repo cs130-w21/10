496ef0d037447f254bd00f93c99176ca7b653957
Jordan Xian Yue Soon <37880824+jordanXYsoon1998@users.noreply.github.com>
2021-02-19 16:58:40 -0800

    Jordan edit profile sprint4 (#66)
    * Skeleton components
    
    * Add function to context to handle both DB write/update and context value update
    
    * Forgot to JSON.stringify before updating context value
    
    * Basic edit name writes to both context and DB
    
    * Forgot that userData is already a JSON str so messed up the DB write
    
    * User data is now a regular object instead of stringified JSON
    
    * Change to Material UI Dialog, TextField and Buttons
    
    * Handle changes more dynamically
    
    * Autocomplete with list of interests from DB working now
    
    * Simple image upload working
    
    * Change Elwyn's Profile no more parse/stringify and get rid of edit popup test page
    
    * Fix bug related to null image upload
    
    * Context listens to DB changes so no need to change context state value
    
    * Pull list of interest suggestions only once from DB, and add another form field for Areas of Expertise

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Login from './components/Login';
 import Test from './components/Test/Test';
 import Home from './components/Test/Home';
 import Profile from './components/Profile';
-import SingleMatchPopup from './components/MatchPage/SingleMatchPopup';
 import MatchPage from './components/MatchPage/MatchPage';
 import ProfileCards from "./components/Swipe";
 function App() {
