2bdc11c72fe0d1c06c6cf2000134723439755402
knguyen99 <khoinguyen@MacBook-Pro-2.lan>
2021-03-07 02:45:25 -0800

    add jsdoc comments

--- src/components/MatchPage/SingleMatchPopup/SingleMatchPopup.js
+++ src/components/MatchPage/SingleMatchPopup/SingleMatchPopup.js
@@ -113,41 +113,6 @@ const SingleMatchPopup = ({ isOpen, onDismiss, userProfile }) => {
           </Grid>  
         </Grid>
       </DialogContent>
-      {/* <div className="profile">
-        <div className="profile-top">
-          <img className="profile-pic"
-            src={userProfile.profile_pic_url}
-            alt={`Profile Pic: ${userProfile.name}`}
-          />
-          <div className="profile-summary">
-            <div className="profile-name">
-              <h4>{userProfile.name}</h4>
-            </div>
-            <div className="profile-company">
-              <h5>{userProfile.work}</h5>
-            </div>
-            <div className="profile-education">
-              <h5>{userProfile.education}</h5>
-            </div>
-          </div>
-        </div>
-        <div className="profile-bottom">
-          {/* <div className="profile-details">
-            <p>{userProfile.details}</p>
-          </div>
-          <div className="profile-interests">
-            <ul>
-              {userProfile.interests.map((interest) =>
-                <li>{interest}</li>
-              )}
-            </ul>
-          </div> 
-          <div className="profile-contact">
-            <div>{`Email: ${userProfile.email}`}</div>
-            <div>{`Phone: ${userProfile.phone}`}</div>
-          </div>
-        </div>
-      </div> */}
     </Dialog>
   );
 };
